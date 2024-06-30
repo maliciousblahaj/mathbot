5d6d9d3ca526267af006a98677e70c44ebffb0d1
maliciousblahaj <maliciousblahaj@gmail.com>
2024-07-01 00:16:23 +0200

    cleanup

--- src/commands/info/ping.rs
+++ src/commands/info/ping.rs
@@ -2,16 +2,12 @@ use mathbot::{get_current_timestamp_millis, send_message, Error, Result, SendCtx
 use mathbot::command::CommandParams;
 use serenity::all::EditMessage;
 
-// temptime = time.time()
-//     msggg = await ctx.send("Pong!")
-//     await msggg.edit(content=f"Pong! Bot latency: {round((time.time()-temptime)*1000)}ms")
-
 pub async fn ping(params: CommandParams) -> Result<()> {
-    //does not work when out of sync with discord's servers
-        //let systemtime = get_current_timestamp_millis()?;
-        //let msg_snowflake = params.msg.id.get();
-        //let msg_timestamp_millis = ((msg_snowflake >> 22) + 1420070400000) as u128;
-        //let ping_time_ms = systemtime - msg_timestamp_millis;
+    //this alternative implementation does not work when not in sync with discord's servers:
+    //  let systemtime = get_current_timestamp_millis()?;
+    //  let msg_snowflake = params.msg.id.get();
+    //  let msg_timestamp_millis = ((msg_snowflake >> 22) + 1420070400000) as u128;
+    //  let ping_time_ms = systemtime - msg_timestamp_millis;
     
     let systemtime = get_current_timestamp_millis()?;
     let mut message = send_message("Pong!", &SendCtx::from_params(&params)).await?;

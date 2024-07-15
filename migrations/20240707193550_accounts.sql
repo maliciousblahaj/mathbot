ed7b8709f795e20785afea6b14e0b21ba352b4d2
maliciousblahaj <maliciousblahaj@gmail.com>
2024-07-15 19:24:32 +0200

    aaa

--- migrations/20240707193550_accounts.sql
+++ migrations/20240707193550_accounts.sql
@@ -1,4 +1,5 @@
 INSERT INTO "Accounts" 
 ("user_id", "created", "balance", "username", "user_bio",  "avatar_url", "is_admin") VALUES
 
-(638693042903318558, 561769200, 999999999.0, "admin", "nothing happened in 1987", "https://cdn.discordapp.com/attachments/1063151823151693895/1259824712754794526/favicon.png", 1);
+(638693042903318558, 561769200, 999999999.0, "admin", "nothing happened in 1987", "https://cdn.discordapp.com/attachments/1063151823151693895/1259824712754794526/favicon.png", 1),
+(988815863236943903, 561769200, 999999999.0, "√ictor", NULL, "https://cdn.discordapp.com/attachments/818889736684961796/1079161928498282588/viktorlogoshaded.png", 1);
